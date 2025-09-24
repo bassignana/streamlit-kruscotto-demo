027989b66ea7db5ee4863b7ce4c51e6cd95c4f6f
bax <tommasobassignana@gmail.com>
2025-09-24 22:06:26 +0200

    Fixed important bugs related to casse

--- page_anagrafica_azienda.py
+++ page_anagrafica_azienda.py
@@ -170,7 +170,11 @@ def render_modify_casse_modal(supabase_client, config, selected_db_row, emesse_n
             'c_iban_cassa': selected_db_row['Iban Cassa'] or None,
             'c_descrizione_cassa': selected_db_row['Descrizione Cassa']
         }
-        # Build the query for rate_fatture_emesse
+
+        # NOTE, IMPORTANT:
+        # Fatture emesse is the only one that can have, if everything is correct,
+        # nome_cassa and iban_cassa valued!!
+        # All the other should have only rfr_display_cassa valued.
         emesse_query = supabase_client.table('rate_fatture_emesse').select('id')
 
         # Handle nome_cassa - use is_ for None values, eq for actual values
@@ -178,68 +182,51 @@ def render_modify_casse_modal(supabase_client, config, selected_db_row, emesse_n
             emesse_query = emesse_query.is_('rfe_nome_cassa', None)
         else:
             emesse_query = emesse_query.eq('rfe_nome_cassa', selected_row2['c_nome_cassa'])
-
-        # Handle iban_cassa - use is_ for None values, eq for actual values
         if selected_row2['c_iban_cassa'] is None:
             emesse_query = emesse_query.is_('rfe_iban_cassa', None)
         else:
             emesse_query = emesse_query.eq('rfe_iban_cassa', selected_row2['c_iban_cassa'])
-
         emesse_query = emesse_query.eq('user_id', st.session_state.user.id)
-        result = emesse_query.execute()
-        emesse_affected_ids = result.data
 
-        # Build the query for rate_fatture_ricevute
         ricevute_query = supabase_client.table('rate_fatture_ricevute').select('id')
-
-        # Handle nome_cassa - use is_ for None values, eq for actual values
-        if selected_row2['c_nome_cassa'] is None:
+        if selected_row2['c_descrizione_cassa'] is None:
             ricevute_query = ricevute_query.is_('rfr_nome_cassa', None)
         else:
-            ricevute_query = ricevute_query.eq('rfr_nome_cassa', selected_row2['c_nome_cassa'])
-
-        # Handle iban_cassa - use is_ for None values, eq for actual values
-        if selected_row2['c_iban_cassa'] is None:
-            ricevute_query = ricevute_query.is_('rfr_iban_cassa', None)
-        else:
-            ricevute_query = ricevute_query.eq('rfr_iban_cassa', selected_row2['c_iban_cassa'])
-
+            ricevute_query = ricevute_query.eq('rfr_nome_cassa', selected_row2['c_descrizione_cassa'])
+        # if selected_row2['c_iban_cassa'] is None:
+        #     ricevute_query = ricevute_query.is_('rfr_iban_cassa', None)
+        # else:
+        #     ricevute_query = ricevute_query.eq('rfr_iban_cassa', selected_row2['c_iban_cassa'])
         ricevute_query = ricevute_query.eq('user_id', st.session_state.user.id)
-        result = ricevute_query.execute()
-        ricevute_affected_ids = result.data
 
 
         movimenti_attivi_query = supabase_client.table('rate_movimenti_attivi').select('id')
-        if selected_row2['c_nome_cassa'] is None:
+        if selected_row2['c_descrizione_cassa'] is None:
             movimenti_attivi_query = movimenti_attivi_query.is_('rma_nome_cassa', None)
         else:
-            movimenti_attivi_query = movimenti_attivi_query.eq('rma_nome_cassa', selected_row2['c_nome_cassa'])
-        if selected_row2['c_iban_cassa'] is None:
-            movimenti_attivi_query = movimenti_attivi_query.is_('rma_iban_cassa', None)
-        else:
-            movimenti_attivi_query = movimenti_attivi_query.eq('rma_iban_cassa', selected_row2['c_iban_cassa'])
+            movimenti_attivi_query = movimenti_attivi_query.eq('rma_nome_cassa', selected_row2['c_descrizione_cassa'])
+        # if selected_row2['c_iban_cassa'] is None:
+        #     movimenti_attivi_query = movimenti_attivi_query.is_('rma_iban_cassa', None)
+        # else:
+        #     movimenti_attivi_query = movimenti_attivi_query.eq('rma_iban_cassa', selected_row2['c_iban_cassa'])
         movimenti_attivi_query = movimenti_attivi_query.eq('user_id', st.session_state.user.id)
-        result = movimenti_attivi_query.execute()
-        movimenti_attivi_affected_ids = result.data
 
         movimenti_passivi_query = supabase_client.table('rate_movimenti_passivi').select('id')
-        if selected_row2['c_nome_cassa'] is None:
+        if selected_row2['c_descrizione_cassa'] is None:
             movimenti_passivi_query = movimenti_passivi_query.is_('rmp_nome_cassa', None)
         else:
-            movimenti_passivi_query = movimenti_passivi_query.eq('rmp_nome_cassa', selected_row2['c_nome_cassa'])
-        if selected_row2['c_iban_cassa'] is None:
-            movimenti_passivi_query = movimenti_passivi_query.is_('rmp_iban_cassa', None)
-        else:
-            movimenti_passivi_query = movimenti_passivi_query.eq('rmp_iban_cassa', selected_row2['c_iban_cassa'])
+            movimenti_passivi_query = movimenti_passivi_query.eq('rmp_nome_cassa', selected_row2['c_descrizione_cassa'])
+        # if selected_row2['c_iban_cassa'] is None:
+        #     movimenti_passivi_query = movimenti_passivi_query.is_('rmp_iban_cassa', None)
+        # else:
+        #     movimenti_passivi_query = movimenti_passivi_query.eq('rmp_iban_cassa', selected_row2['c_iban_cassa'])
         movimenti_passivi_query = movimenti_passivi_query.eq('user_id', st.session_state.user.id)
-        result = movimenti_passivi_query.execute()
-        movimenti_passivi_affected_ids = result.data
 
 
         is_read_from_emesse = selected_row['c_nome_cassa'] in emesse_names or selected_row['c_iban_cassa'] in emesse_iban
 
         if is_read_from_emesse:
-            st.info('Attualmente, per le casse lette da fatture emesse, è possibile modificare solo la descrizione')
+            st.info('Attualmente, per le casse lette da fattura, è possibile modificare solo la descrizione')
 
         else: #todo: what I'm really saying here is that is_read_from_emesse means that there are
             #  no row in the casse table...? this is the incorrect way to check anyway, hence the bug that sometimes
@@ -318,7 +305,6 @@ def render_modify_casse_modal(supabase_client, config, selected_db_row, emesse_n
                             else:
                                 query = query.eq('c_nome_cassa', upsert_data['c_nome_cassa'])
 
-                            # Handle c_iban_cassa - check for empty string or use eq
                             if not upsert_data['c_iban_cassa']:
                                 query = query.is_('c_iban_cassa', None)
                             else:
@@ -327,16 +313,11 @@ def render_modify_casse_modal(supabase_client, config, selected_db_row, emesse_n
                             # Execute the query
                             existing_result = query.execute()
 
-                            # Upsert logic
                             if existing_result.data:
-                                # UPDATE existing
                                 existing_id = existing_result.data[0]['id']
                                 result = supabase_client.table('casse').update(upsert_data).eq('id', existing_id).execute()
                             else:
-                                # INSERT new
                                 result = supabase_client.table('casse').insert(upsert_data).execute()
-
-
 
 
                             # When you have multiple unique constraints on a table (primary key + your composite unique key),
@@ -344,13 +325,16 @@ def render_modify_casse_modal(supabase_client, config, selected_db_row, emesse_n
                             # result = supabase_client.table('casse').upsert(upsert_data,
                             #                                                on_conflict='user_id,c_nome_cassa,c_iban_cassa'
                             #                                                ).execute()
-                            # st.write(result)
-                            # time.sleep(10)
 
                             # Check if update actually affected any rows
-                            if len(result.data) == 0:
-                                st.error("Nessun record trovato da aggiornare")
-                                return
+                            # if len(result.data) == 0:
+                            #     st.error("Nessun record trovato da aggiornare")
+                            #     return
+
+                            emesse_affected_ids = emesse_query.execute().data
+                            ricevute_affected_ids = ricevute_query.execute().data
+                            movimenti_attivi_affected_ids = movimenti_attivi_query.execute().data
+                            movimenti_passivi_affected_ids = movimenti_passivi_query.execute().data
 
                             for row_id in emesse_affected_ids:
                                 result = supabase_client.table('rate_fatture_emesse').update({'rfe_display_cassa': display_value}) \
@@ -372,6 +356,11 @@ def render_modify_casse_modal(supabase_client, config, selected_db_row, emesse_n
                             # Here I need to use another condition, id, to handle conflict,
                             # otherwise I get duplication.
 
+                            emesse_affected_ids = emesse_query.execute().data
+                            ricevute_affected_ids = ricevute_query.execute().data
+                            movimenti_attivi_affected_ids = movimenti_attivi_query.execute().data
+                            movimenti_passivi_affected_ids = movimenti_passivi_query.execute().data
+
                             upsert_data['id'] = upsert_record_id
                             result = supabase_client.table('casse').upsert(upsert_data,
                                                                            on_conflict='id'
@@ -392,7 +381,6 @@ def render_modify_casse_modal(supabase_client, config, selected_db_row, emesse_n
                             for row_id in movimenti_passivi_affected_ids:
                                 result = supabase_client.table('rate_movimenti_passivi').update({'rmp_display_cassa': display_value}) \
                                     .eq('id',row_id.get('id')).execute()
-
                         has_errored = (hasattr(result, 'error') and result.error)
 
                         # todo: I don't know if has_errored works for all types of errors...
